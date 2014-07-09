135b68236ca7494f1b80a504a5b2cbfbf83f4d5a
tsean <tsean>
2014-07-09 15:11:42 +0800

    add icon and .gitignore

---
+++ BabyDiary/BabyDiary/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BabyDiary
+//
+//  Created by 楊 德忻 on 2014/7/8.
+//  Copyright (c) 2014年 德忻 楊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
