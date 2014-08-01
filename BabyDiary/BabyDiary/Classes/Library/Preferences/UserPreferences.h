6fe2bb11d2f36f741f6e0f9ce312cf9c520c7267
dsean <sean.yang@qlync.com>
2014-08-01 11:41:22 +0800

    to add UserPreferences save username and password

---
+++ BabyDiary/BabyDiary/Classes/Library/Preferences/UserPreferences.h
@@ -0,0 +1,22 @@
+//
+//  UserPreferences.h
+//  BabyDiary
+//
+//  Created by 楊 德忻 on 2014/8/1.
+//  Copyright (c) 2014年 德忻 楊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserPreferences : NSObject
+
+@property (nonatomic, copy) NSString *defaultsUsername;
+@property (nonatomic, copy) NSString *defaultsPassword;
+
++ (id)sharedUserPreferences;
+- (void)tosetUsername:(NSString *)username;
+- (void)tosetPassword:(NSString *)password;
+- (NSString *)togetUsername;
+- (NSString *)togetPassword;
+
+@end
---
+++ BabyDiary/BabyDiary/Classes/Library/Preferences/UserPreferences.m
@@ -0,0 +1,13 @@
+//
+//  UserPreferences.m
+//  BabyDiary
+//
+//  Created by 楊 德忻 on 2014/8/1.
+//  Copyright (c) 2014年 德忻 楊. All rights reserved.
+//
+
+#import "UserPreferences.h"
+
+@implementation UserPreferences
+
+@end
