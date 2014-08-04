01e2310418da76fd911f2a34c67b48e3a5f3d142
dsean <sean.yang@qlync.com>
2014-08-04 11:51:18 +0800

    change Variables name and add hasAccount method . add comment.

--- BabyDiary/BabyDiary/AppDelegate/AppDelegate.m
+++ BabyDiary/BabyDiary/AppDelegate/AppDelegate.m
@@ -8,66 +8,72 @@
 
 #import "AppDelegate.h"
 #import "SatManager.h"
-#import "UserPreferences.h"
 #import "LoginHandler.h"
+#import "UserPreferences.h"
+
 
 @interface AppDelegate ()
 
 @property (nonatomic, copy) NSString *defaultusername;
 @property (nonatomic, copy) NSString *defaultpassword;
-@property (nonatomic) BOOL checkDefaultValuesTiLogin;
-@property (nonatomic) BOOL checkLoginStatus;
+@property (nonatomic) BOOL isDefaultValuesLogin;
+@property (nonatomic) BOOL isLoginStatus;
 
 @end
 
 @implementation AppDelegate
 
+#pragma mark-lifCycle
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
-    
     NSString *licensePath = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"license"];
     [SatManager setLicensePath:licensePath];
     
-    _defaultusername = [[UserPreferences sharedUserPreferences] togetUsername];
-    _defaultpassword = [[UserPreferences sharedUserPreferences] togetPassword];
-    if (_defaultusername == NULL || _defaultpassword == NULL) {
-        _checkDefaultValuesTiLogin = NO;
+    if (![self hasAccount]) {
+        self.isDefaultValuesLogin = NO;
     }
     else {
-         _checkDefaultValuesTiLogin = YES;
-        _checkLoginStatus = [LoginHandler satLogin:_defaultusername :_defaultpassword];
+         self.isDefaultValuesLogin = YES;
+        
+        // this is a blocking method
+        self.isLoginStatus = [LoginHandler satLogin:_defaultusername :_defaultpassword];
     }
     return YES;
 }
 							
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+- (void)applicationWillResignActive:(UIApplication *)application {
+    
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    if (_checkDefaultValuesTiLogin) {
-        if (_checkLoginStatus) {
+    if (self.isDefaultValuesLogin) {
+        if (self.isLoginStatus) {
             [self.window.rootViewController performSegueWithIdentifier:@"loginSegue" sender:self.window.rootViewController];
         }
     }
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+- (void)applicationWillTerminate:(UIApplication *)application {
+    
+}
+
+#pragma mark-toolMethods
+
+- (BOOL)hasAccount {
+    self.defaultusername = [[UserPreferences sharedUserPreferences] togetUsername];
+    self.defaultpassword = [[UserPreferences sharedUserPreferences] togetPassword];
+    if (_defaultusername == NULL || _defaultpassword == NULL) {
+        return NO;
+    }
+    return YES;
 }
 
 @end
